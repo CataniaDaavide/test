cbcb1ea99c498e58176cb0735a6f6c31fcd18419
Davide Catania <catania561@gmail.com>
2025-08-20 15:55:07 +0200

    Initial commit from Create Next App

---
+++ src/middleware.js
@@ -0,0 +1,26 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export default async function middleware(request) {
+  const routesPrivate = ["/profile", "/dashboard", "/dashboard/categories", "/dashboard/transictions", "/dashboard/wallet", "/dashboard/profile"]
+  const isPrivate = routesPrivate.includes(request.nextUrl.pathname)
+  
+  const cookieStore = await cookies()
+  const token = cookieStore.get("token")?.value
+  
+  const url = `${request.nextUrl.origin}/api/auth/verify-token`;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({token}),
+  };
+
+  if (isPrivate) {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
+}
