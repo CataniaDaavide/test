2f57cb56ed2b064e807d932ca1a78e9a01e14c74
Davide Catania <catania561@gmail.com>
2025-08-25 10:14:45 +0200

    update

--- src/app/(pages)/page.jsx
+++ src/app/(pages)/page.jsx
@@ -45,14 +45,14 @@ export default function App() {
       <Card>
         <div className="w-full flex gap-3 grid-cols-3">
         <Input
-          // icon={<Calendar />}
+          icon={<Calendar />}
           title={"Data"}
           type={"date"}
           name={"data"}
           ref={dateRef}
         />
         <Input
-          // icon={<Clock />}
+          icon={<Clock />}
           title={"Time"}
           type={"time"}
           name={"time"}
