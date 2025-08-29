070667ea124cfbd80497823e4d51e90df87af4f2
Davide Catania <catania561@gmail.com>
2025-08-29 09:22:15 +0200

    update

--- src/app/components/ui/modal/modal.jsx
+++ src/app/components/ui/modal/modal.jsx
@@ -54,7 +54,7 @@ export default function Modal() {
       className="absolute top-0 left-0 z-[999] w-[100dvw] h-[100dvh] flex items-center justify-center md:p-3 bg-black/50"
       onClick={handleOverlayClick} 
     >
-      <Card className="relative h-full md:h-auto !rounded-none md:!rounded-xl md:max-w-md !justify-start">
+      <Card className="relative h-[100dvh] !border-0 md:!border-1 md:h-auto !rounded-none md:!rounded-xl md:max-w-md !justify-start">
         {ModalComponent}
       </Card>
     </div>
