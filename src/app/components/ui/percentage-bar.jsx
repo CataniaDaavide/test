13a68b3c9df9afa801865ad5f93dee1ecbe116dd
Davide Catania <catania561@gmail.com>
2025-09-12 11:54:16 +0200

    update

--- src/app/components/ui/percentage-bar.jsx
+++ src/app/components/ui/percentage-bar.jsx
@@ -1,13 +1,20 @@
+"use client"
+import { useState } from "react";
 import TitleComponents from "./title-components";
 
 export default function PercentageBar({ titleSx, titleDx, percentage, color }) {
+  const [ss, s] = useState()
   return (
     <div className="w-full h-full flex flex-col">
       <div className="flex items-center justify-between">
         <TitleComponents>{titleSx}</TitleComponents>
+        {ss && <p>ciao</p>}
         <TitleComponents>{titleDx}</TitleComponents>
       </div>
-      <div className="w-full h-[10px] rounded-full bg-zinc-200 dark:bg-zinc-700">
+      <div 
+      onTouchStart={() => {s(true)}}
+      onTouchEnd={() => {s(false)}}
+      className="w-full h-[10px] rounded-full bg-zinc-200 dark:bg-zinc-700">
         <div
           className={`h-full rounded-full bg-background-inverse`}
           style={{ width: `${percentage}%` }}
