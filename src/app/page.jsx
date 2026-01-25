13bd1f47a6b07ab92075a2003790738c36397e7b
CataniaDaavide <catania561@gmail.com>
2026-01-25 14:07:07 +0100

    update

---
+++ src/app/page.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full overflow-auto">
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+     <div className="w-full h-32 border-10 border-zinc-800" />
+    </div>
+  );
+}
